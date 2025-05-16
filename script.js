63ddd6e22d8db4428cf30ea181c8bad87af6ab9f
AshokNandu-11 <114278147+AshokNandu-11@users.noreply.github.com>
2025-05-16 08:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) return [];
+  // Initialize an empty array to store the chunks
+  let chunks = [];
+  // Loop through the string with steps of 'size'
+  for (let i = 0; i < str.length; i = i + size) {
+    // Extract a substring from the current position up to i+size (but not exceeding the string length)
+    let subStr = str.slice(i, i + size);
+    // Add the substring chunk to the chunks array
+    chunks.push(subStr);
+  }
+  // Return the array containing all the chunks
+  return chunks;
 }
 
 // Do not change the code below
